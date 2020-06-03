8a40e1b12d98eb2b9f58d1af112b1bcd9aca3f7a
lunoos <pmahendar2373@gmail.com>
2020-06-03 15:09:17 +0530

    Updates

---
+++ precache-manifest.13420839a955925a1732258d9cbf4f8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8cf61f5ce2e5da523bf23286d8031e5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c0d12013a715c2f21f33",
+    "url": "/robofriends/static/css/main.7f646e8d.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d12013a715c2f21f33",
+    "url": "/robofriends/static/js/main.a296796a.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "d0ead76c79aab05fcf13a37b41fc9107",
+    "url": "/robofriends/static/media/pee-on-your-face.d0ead76c.woff"
+  }
+]);
